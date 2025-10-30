09472bf17fc403496e09b227cb727fc0e6fa35da
owned_dragon <abhinavkatkera@gmail.com>
2025-10-30 21:05:49 +0530

    inital commit

---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/owned-dragon/video-downloader/backend.git
+
+go 1.25.3
+
+require (
+	github.com/gdamore/encoding v1.0.1 // indirect
+	github.com/gdamore/tcell/v2 v2.8.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gosuri/uilive v0.0.4 // indirect
+	github.com/gosuri/uiprogress v0.0.1 // indirect
+	github.com/k0kubun/go-ansi v0.0.0-20180517002512-3bf9e2903213 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/tview v0.42.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/schollz/progressbar/v3 v3.18.0 // indirect
+	golang.org/x/sys v0.37.0 // indirect
+	golang.org/x/term v0.36.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/k0kubun/go-ansi"
+	"github.com/schollz/progressbar/v3"
+)
+
+func generateFileName() string {
+	currTime := time.Now().Format("20060102150405")
+	id := uuid.New()
+	fileName := fmt.Sprintf("%s_%s_video", currTime, id)
+	return fileName
+}
+
+func runCommand(command_name string, url string) error {
+
+	if err := os.MkdirAll("downloads", 0755); err != nil {
+		return nil
+	}
+
+	fileName := generateFileName()
+
+	videoPath := filepath.Join("downloads", fileName)
+
+	fmt.Printf("Running: %s %v\n", command_name, url)
+	cmd := exec.Command(command_name,
+		"-f", "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4",
+		"-o", videoPath, url,
+		"--no-warnings", "--progress",
+	)
+
+	stderr, err := cmd.StderrPipe()
+
+	if err != nil {
+		return err
+	}
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	bar := progressbar.NewOptions(1000,
+		progressbar.OptionSetWriter(ansi.NewAnsiStdout()), //you should install "github.com/k0kubun/go-ansi"
+		progressbar.OptionEnableColorCodes(true),
+		progressbar.OptionShowBytes(true),
+		progressbar.OptionSetWidth(15),
+		progressbar.OptionSetDescription("[cyan][1/3][reset] Downloading..."),
+		progressbar.OptionSetTheme(progressbar.Theme{
+			Saucer:        "[green]=[reset]",
+			SaucerHead:    "[green]>[reset]",
+			SaucerPadding: " ",
+			BarStart:      "[",
+			BarEnd:        "]",
+		}))
+
+	regrexPercentage := regexp.MustCompile(`([0-9]+\.[0.9]+)%`)
+	scanner := bufio.NewScanner(stderr)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+		if match := regrexPercentage.FindStringSubmatch(line); len(match) == 2 {
+			var percentage float64
+			fmt.Sscanf(match[1], "%f", &percentage)
+			_ = bar.Set(int(percentage))
+		}
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return fmt.Errorf("yt-dlp failed: %w", err)
+	}
+
+	_ = bar.Finish()
+	fmt.Println("\n✅ MP4 download complete:", videoPath)
+	return nil
+
+}
+
+func main() {
+	url := os.Args[1]
+
+	if err := runCommand("yt-dlp", url); err != nil {
+		fmt.Errorf(err.Error())
+	}
+}
